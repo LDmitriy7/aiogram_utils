5430a7dd1d036ad26d335a02bf2697e9da2884ab
levch <levchenko.d.a1998@gmail.com>
2022-01-25 14:06:12 +0300

    v 0.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name='aiogram_utils',
-    version='0.0.9',
+    version='0.0.11',
     url='https://github.com/LDmitriy7/aiogram_utils',
 
     python_requires='>=3.7',
